04024cc9a43fb47ac892e6a974ca00b63b993aa0
shreeraam-sys <shreeraamr@gmail.com>
2024-05-09 02:56:54 -0400

    Code implementation for stack with linkedlist

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,6 +1,13 @@
-public class StackAsLinkedList { 
+class StackAsLinkedList { 
   
-    StackNode root; 
+    StackNode root; //represents head of the singlyLinked list
+
+    /*
+    Represents the last node of the singly linked list
+    this is used to push new elements to the stack
+    by adding a top node element to the stack, we can do the push(), peek() and operations in O(1) time
+    */ 
+    StackNode top; 
   
     static class StackNode { 
         int data; 
@@ -9,18 +16,29 @@ public class StackAsLinkedList {
         StackNode(int data) 
         { 
             //Constructor here 
+            this.data = data;
         } 
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        //Write your code here for the condition if stack is empty.
+       return root == null;
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+        StackNode stackNode = new StackNode(data);
+        if(root == null){
+            root = stackNode;
+            top = root;
+        }else{
+            //
+            top.next = stackNode;
+            top = stackNode;
+        }
     } 
   
     public int pop() 
@@ -28,11 +46,39 @@ public class StackAsLinkedList {
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+
+        if(root == null){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+
+
+        StackNode node = root;
+        StackNode prevNode = null;
+
+        while(node.next != null){
+            prevNode = node;
+            node = node.next;
+        }
+
+        if(prevNode != null){
+            prevNode.next = null;
+        }
+        top = prevNode;
+        return node.data;
+
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+
+        if(root == null){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+
+        return top.data;
     } 
   
 	//Driver code
